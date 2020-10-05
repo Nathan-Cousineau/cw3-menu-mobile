d4d4fbc5f8755bc4604a1596cc39d74720acba09
Nathan-Cousineau <nathan.cousineau@yahoo.ca>
2020-10-05 14:52:35 -0400

    15 min video

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
-    //Un commentaire
     console.log("Ça fonctionne!!!");
+
+    var hamburgers = document.querySelectorAll('.hamburger');
+
+
+    for(let i = 0; i < hamburgers.length; i++) {
+        var hamburger = hamburgers[i];
+        hamburger.addEventListener('click', openMenu);
+
+    }
+
+    function openMenu(evt) {
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+
+        if(cible.classList.contains('open')){
+            cible.classList.remove('open');
+        } else {
+            cible.classList.add('open');
+        }
+
+    }
 
 });
